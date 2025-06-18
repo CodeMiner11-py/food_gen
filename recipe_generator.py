3320a947cc289a23b0924db1f8ffc7af8ea240b9
Eshaan Buddhisagar <eshaan@mac.lan>
2025-06-18 12:24:11 -0700

    measuring unit clarification

--- recipe_generator.py
+++ recipe_generator.py
@@ -185,6 +185,7 @@ THIS INFORMATION is vital for our code.
 Even if you can't give a complete answer, please try your best.
 PLEASE DO NOT SPEAK ANY TEXT OTHER THAN THE FORMATTED NUTRITION RESPONSE. THIS WILL BREAK THE CODE.
 DO NOT INCLUDE SOMETHING LIKE 5-10 MG. JUST SAY 7.5 MG AS AN AVERAGE. DO NOT SPEAK RANGES.
+Nutritional facts should be given with their appropriate measurement units, like cal for calories or mg for sodium.
 Thanks a lot for you complying.
 The recipe is below, and is structured into title;description;ingredients;procedure;random. Ignore the last 'random' part:
 {recipe_text}
